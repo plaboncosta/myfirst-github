be649f3c444c577c43e8aa1f7a4a577cf974cdb6
Plabon <plabonjoseph@gmail.com>
2019-02-04 17:38:28 +0600

    Add some feature in fourth lesson and check js function scope

--- custom.js
+++ custom.js
@@ -1,4 +1,44 @@
+// Give Answer Function
 function answer()
 {
     document.getElementById('answer').innerHTML = 'Yea, Of Course!';
 }
+
+
+// Give Addition Result
+function addition(firstNumber = 5, secondNumber = 5)
+{
+    var result = firstNumber + ' and ' + secondNumber + ' is = ' + (firstNumber + secondNumber);
+    document.getElementById('addition').innerHTML = result;
+    // alert(firstNumber + secondNumber);
+    // console.log(firstNumber + secondNumber);
+}
+
+
+// Show Name
+function showName($name = 'Plabon')
+{
+    document.getElementById('show_name').innerHTML = $name;
+}
+
+
+// Checking scope js funciton
+var name = 'Plabon',
+title = ' Costa';
+function myName()
+{
+    // var name = 'Plabon';
+    var religionName = 'Joseph';
+    return (function()
+    {
+        // var title = ' Costa';
+        return (function()
+        {
+            return name + title;
+        })();
+    })();
+}
+
+// console.log(religionName); (You can not aceess this because this variable is called inside the function)
+
+
